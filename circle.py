81a8ed80e5b5eab539939880f920d2f2245ed264
yallmoong <victoria0404@naver.com>
2023-09-26 01:52:16 +0900

    폴더 정리

---
+++ circle.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape('turtle')
+
+turtle.penup()
+turtle.goto(200, 200)
+turtle.pendown()
+turtle.circle(50)
+turtle.write("(200, 200)")
+
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+turtle.circle(50)
+turtle.write("(-200, -200)")
+
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.circle(30)
+turtle.write("home")
+
+turtle.exitonclick()
